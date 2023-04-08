77829a9723401a1d4dcb14a36b4475d70155b3d2
Eceidaci <129887428+Eceidaci@users.noreply.github.com>
2023-04-08 16:26:06 +0300

    quiz soru-cevap scripti

---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<QuestionsAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+
+    public TextMeshProUGUI QuestionTxt;
+
+
+    private void Start()
+    {
+        generateQuestion();
+    }
+
+    public void correct()
+    {
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+
+    void SetAnswers()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+           
+            options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = QnA[currentQuestion].Answers[i];
+
+            if (QnA[currentQuestion].CorrectAnswer == i+1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+         
+        }
+
+    }
+
+
+    void generateQuestion()
+    {
+        currentQuestion = Random.Range(0, QnA.Count);
+       
+        QuestionTxt.text = QnA[currentQuestion].Question;
+
+        SetAnswers();
+       
+    }
+}
+
