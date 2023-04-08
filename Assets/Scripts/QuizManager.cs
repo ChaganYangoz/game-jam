0e45dab7b3dbf0d8464afac883b78f8041855f32
Eceidaci <129887428+Eceidaci@users.noreply.github.com>
2023-04-08 18:05:06 +0300

    quiz gem script son hal

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class QuizManager : MonoBehaviour
 {
@@ -10,26 +11,70 @@ public class QuizManager : MonoBehaviour
     public GameObject[] options;
     public int currentQuestion;
 
+    public GameObject Quizpanel;
+    public GameObject GoPanel;
+
 
     public TextMeshProUGUI QuestionTxt;
+    public TextMeshProUGUI ScoreTxt;
+
+    int totalQuestions = 0;
+    public int score;
 
 
     private void Start()
     {
+        totalQuestions = QnA.Count;
+        GoPanel.SetActive(false);
         generateQuestion();
     }
 
+    public void retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void GameOver()
+    {
+        Quizpanel.SetActive(false);
+        GoPanel.SetActive(true);
+        ScoreTxt.text = score + "/" + totalQuestions;
+    }
+
+
     public void correct()
     {
+        //when u are right
+        score += 1;
         QnA.RemoveAt(currentQuestion);
+        StartCoroutine(WaitForNext());
+       
+        
+    }
+
+    public void wrong()
+    {
+        //when u answer wrong
+        QnA.RemoveAt(currentQuestion);
+        StartCoroutine(WaitForNext());
+        
+        
+    }
+
+    IEnumerator WaitForNext()
+    {
+        yield return new WaitForSeconds(1);
         generateQuestion();
     }
+
 
 
     void SetAnswers()
     {
         for (int i = 0; i < options.Length; i++)
         {
+            options[i].GetComponent<Image>().color = options[i].GetComponent<AnswerScript>().startColor;
+
             options[i].GetComponent<AnswerScript>().isCorrect = false;
            
             options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = QnA[currentQuestion].Answers[i];
@@ -46,11 +91,19 @@ public class QuizManager : MonoBehaviour
 
     void generateQuestion()
     {
-        currentQuestion = Random.Range(0, QnA.Count);
-       
-        QuestionTxt.text = QnA[currentQuestion].Question;
+        if(QnA.Count > 0)
+        {
+            currentQuestion = Random.Range(0, QnA.Count);
 
-        SetAnswers();
+            QuestionTxt.text = QnA[currentQuestion].Question;
+
+            SetAnswers();
+        }
+        else
+        {
+            Debug.Log("Out of Question");
+            GameOver();
+        }
        
     }
 }
