bd3582be366ea99183b5534594479297a3169c3a
ChaganYangoz <caganyangoz@gmail.com>
2023-04-09 23:43:56 +0300

    boss

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -17,7 +18,7 @@ public class PlayerHealth : MonoBehaviour
         health=health- damage;
         if (health <= 0)
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
